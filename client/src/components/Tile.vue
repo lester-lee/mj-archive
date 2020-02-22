9a5a8c6834548f9a1e2fe68a5dd8ac31385a871d
Lester Lee <ll6@williams.edu>
2020-02-21 22:21:46 -0500

    Add untracked files

---
+++ client/src/components/Tile.vue
@@ -0,0 +1,31 @@
+<template>
+  <li class="Tile">
+    <img :src="tile.url" :alt="tile.suit + ':' + tile.rank"/>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ['tile']
+}
+</script>
+
+<style lang="scss">
+.Tile{
+  user-select: none;
+  display: inline-block;
+  width: 50px;
+  img {
+    max-width: 100%;
+  }
+  &.canHover:hover{
+    cursor: pointer;
+    position: relative;
+    top: -5px;
+  }
+  &.canHover:active{
+    top: -10px;
+  }
+}
+
+</style>
---
+++ client/src/io.js
@@ -0,0 +1,27 @@
+export default function attachListeners(socket, store) {
+  socket.emit('new game', 'id1234');
+  socket.on('new game', game => {
+    store.gameId = game.id;
+    socket.emit('update tiles', {
+      gameId: game.id,
+      playerNum: store.playerNum,
+    });
+  });
+
+  socket.on('update hand', hands => {
+    store.hands = hands;
+  });
+
+  socket.on('update melds', melds => {
+    store.melds = melds;
+  });
+
+  socket.on('update discard', pile => {
+    store.pile = pile;
+  });
+
+  socket.on('update turn', curPlayer => {
+    store.myTurn = store.playerNum === curPlayer;
+  });
+
+}
