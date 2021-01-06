cec76d62b58cc271c7f31b6b141a3aed301fdf38
Lester Lee <ll6@williams.edu>
2021-01-06 15:02:05 -0500

    Fix typo in build.sh

--- client/build.sh
+++ client/build.sh
@@ -1,4 +1,4 @@
 #/bin/bash
-npm install —only=dev # installs only dev dependencies
-npm install            # installs prod dependencies
-npm run build          # builds the Vue.js app
+npm install --only=dev  # installs only dev dependencies
+npm install
+npm run build
